6dcd860ea339e522b08a68bef5276188027a9dc6
Dario Izzo <dario.izzo@googlemail.com>
2011-02-04 12:03:32 +0100

    examples directory/module removed

--- PyKEP/__init__.py
+++ PyKEP/__init__.py
@@ -1,8 +1,7 @@
 import sims_flanagan
-import examples
 
 __doc__ = 'PyKEP is the answer'
-__all__ = ['core', 'sims_flanagan', 'examples']
+__all__ = ['core', 'sims_flanagan']
 
 # For convenience, bring all core classes into the root namespace when importing *.
 from core import *
