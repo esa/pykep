9b84cc4236868cbeffc0381ae2d5b172a7665935
Dario Izzo <dario.izzo@googlemail.com>
2012-02-10 14:11:23 +0100

    plot_kepler added to PyKEP

--- PyKEP/core/_plots.py
+++ PyKEP/core/_plots.py
@@ -1,14 +1,14 @@
-def plot_planet(ax,plnt,t0='PyKEP.epoch(0)', N=60, units = 'PyKEP.AU', color = 'k', legend = False):
+def plot_planet(ax,plnt,t0='PyKEP.epoch(0)', N=60, units = 1.0, color = 'k', legend = False):
 	"""
 	Plots the planet position and its orbit
 		      
 	USAGE: plot_planet(ax,plnt,t0='PyKEP.epoch(0)', N=60, units=AU, legend = False):
-	  * ax = 3D axis object created using fig.gca(projection='3d')
-	  * plnt = PyKEP.planet object we want to plot
-	  * N = number of points to be plotted along one orbit
-	  * t0 = PyKEP.epoch object indicating when we want to plot the planet position
-	  * units = in meters. The length unit to be used in the plot
-	  * legend = when True it plots also the legend with the planet name
+	  * ax:		3D axis object created using fig.gca(projection='3d')
+	  * plnt:	PyKEP.planet object we want to plot
+	  * t0:		PyKEP.epoch object indicating when we want to plot the planet position
+	  * units:	the length unit to be used in the plot
+	  * color:	matplotlib color to use to plot the line
+	  * legend	when True it plots also the legend with the planet name
 	  
 	EXAMPLE:
 	  from mpl_toolkits.mplot3d import Axes3D
@@ -26,8 +26,6 @@ def plot_planet(ax,plnt,t0='PyKEP.epoch(0)', N=60, units = 'PyKEP.AU', color = '
 	
 	if t0 == 'PyKEP.epoch(0)':
 		t0 = epoch(0)
-	if units == 'PyKEP.AU':
-		units = AU
 	
 	#orbit semi-major axis
 	a = plnt.orbital_elements[0]
@@ -56,46 +54,49 @@ def plot_planet(ax,plnt,t0='PyKEP.epoch(0)', N=60, units = 'PyKEP.AU', color = '
 	if legend:
 		ax.legend()
 	
-def plot_lambert(ax,l, N=60, sol=0, units = 'PyKEP.AU', color = 'b', legend = False):
+def plot_lambert(ax,l, N=60, sol=0, units = 1.0, color = 'b', legend = False):
 	"""
 	Plots a particular solution to a Lambert's problem
 	      
 	USAGE: plot_lambert(ax,l, N=60, sol=0, units = 'PyKEP.AU', legend = 'False')
-	  * ax = 3D axis object created using fig.gca(projection='3d')
-	  * l = PyKEP.lambert_problem object
-	  * N = number of points to be plotted along one arc
-	  * sol = solution to the Lambert's problem we want to plot (must be in 0..Nmax*2)
-	          where Nmax is the maximum number of revolutions for which there exist a solution.
-	  * units = in meters. The length unit to be used in the plot
-	  * legend = when True it plots also the legend with info on the sol plotted
+	  * ax:		3D axis object created using fig.gca(projection='3d')
+	  * l:		PyKEP.lambert_problem object
+	  * N:		number of points to be plotted along one arc
+	  * sol:	solution to the Lambert's problem we want to plot (must be in 0..Nmax*2)
+			where Nmax is the maximum number of revolutions for which there exist a solution.
+	  * units:	the length unit to be used in the plot
+	  * color:	matplotlib color to use to plot the line
+	  * legend	when True it plots also the legend with info on the Lambert's solution chosen
 	  
 	EXAMPLE:
 	  from mpl_toolkits.mplot3d import Axes3D
 	  import matplotlib.pyplot as plt
-	  
+	
+	  fig = plt.figure()
+	  ax = fig.gca(projection='3d')
+
 	  t1 = epoch(0)
-	  t2 = epoch(540)
+	  t2 = epoch(640)
 	  dt = (t2.mjd2000 - t1.mjd2000) * DAY2SEC
-  
+
 	  pl = planet_ss('earth')
-	  plot_planet(ax,pl, t0=t1)
+	  plot_planet(ax,pl, t0=t1, color='k')
 	  rE,vE = pl.eph(t1)
 
 	  pl = planet_ss('mars')
-	  plot_planet(ax,pl, t0=t2)
+	  plot_planet(ax,pl, t0=t2, color='r')
 	  rM, vM = pl.eph(t2)
 
 	  l = lambert_problem(rE,rM,dt,MU_SUN)
-	  plot_lambert(ax,l, sol=0)
-	  plot_lambert(ax,l, sol=1)
+	  plot_lambert(ax,l, color='b')
+	  plot_lambert(ax,l,sol=1, color='g')
+	  plot_lambert(ax,l,sol=2, color='g')
 
 	  plt.show()
 	"""
 	from PyKEP import propagate_lagrangian, AU
 	import numpy as np
 	
-	if units == 'PyKEP.AU':
-		units = AU
 		
 	if sol > l.get_Nmax()*2:
 		raise ValueError("sol must be in 0 .. NMax*2 \n * Nmax is the maximum number of revolutions for which there exist a solution to the Lambert's problem \n * You can compute Nmax calling the get_Nmax() method of the lambert_problem object")
@@ -126,3 +127,56 @@ def plot_lambert(ax,l, N=60, sol=0, units = 'PyKEP.AU', color = 'b', legend = Fa
 	
 	if legend:
 		ax.legend()
+
+def plot_kepler(ax,r,v,t,mu, N=60, units = 1, color = 'b', legend = False):
+	"""
+	Plots the result of a keplerian propagation
+		      
+	USAGE: plot_kepler(ax,r,v,t,mu, N=60, units = 1, color = 'b', legend = False):
+	  * ax:		3D axis object created using fig.gca(projection='3d')
+	  * r:		initial position (cartesian coordinates)
+	  * v:		initial velocity (cartesian coordinates)
+	  * t:		propagation time
+	  * mu:		gravitational parameter 
+	  * N:		number of points to be plotted along one arc
+	  * units:	the length unit to be used in the plot
+	  * color:	matplotlib color to use to plot the line
+	  * legend	when True it plots also the legend
+	  
+	EXAMPLE:
+	  from mpl_toolkits.mplot3d import Axes3D
+	  import matplotlib.pyplot as plt
+	
+	  fig = plt.figure()
+	  ax = fig.gca(projection='3d')
+	  pl = planet_ss('earth')
+	  plot_planet(ax,pl)
+	  plt.show()
+	"""
+	
+	from PyKEP import MU_SUN, SEC2DAY, epoch, AU
+	from math import pi,sqrt
+	import numpy as np
+  
+	#We define the integration time ...
+	dt = t / (N-1)
+	
+	#... and calcuate the cartesian components for r
+	x = np.array([0.0]*N)
+	y = np.array([0.0]*N)
+	z = np.array([0.0]*N)
+	
+	#We calculate the spacecraft position at each dt
+	for i in range(N):
+		x[i] = r[0]/units
+		y[i] = r[1]/units
+		z[i] = r[2]/units
+		r,v = propagate_lagrangian(r,v,dt,mu)
+	
+	#And we plot
+	ax.plot(x, y, z, c=color, label='ballistic arc')
+	
+	if legend:
+		ax.legend()
+  
+	
