01f72936263742628ea66486ae1a6264c1408cd2
Dario Izzo <darioizzo@googlemail.com>
2012-08-20 16:01:05 +0200

    Restored python converter registration. The code below, which seemed to solve the nasty warning turned out not to work!!
    boost::python::type_info info = boost::python::type_id<T >();
    const boost::python::converter::registration* reg = boost::python::converter::registry::query(info);
    if (reg == NULL)
    {
    	to_tuple_mapping<T >();
    	from_python_sequence<T,policy>();
    }

--- PyKEP/__init__.py
+++ PyKEP/__init__.py
@@ -1,9 +1,12 @@
+import core, sims_flanagan, examples
 __doc__ = 'PyKEP is the answer'
 __all__ = ['core', 'sims_flanagan', 'orbit_plots', 'examples']
 
+# For convenience, bring all core classes into the root namespace when importing *.
+from core import *
+
 """Detecting Installed Extensions"""
 # Fill up the __extensions__ variable with all detected extensions
-
 __extensions__ = {'matplotlib': False, 'mplot3d': False,'pygmo': False}
 try:
 	from matplotlib import __version__ as matplotlib_ver
@@ -25,11 +28,6 @@ except:
 	pass
 
 __version__ = '1.1.2'
-
-# For convenience, bring all core classes into the root namespace when importing *.
-from core import *
-import sims_flanagan
-import examples
 
 #Importing dependent modules
 if (__extensions__['matplotlib'] == True):
