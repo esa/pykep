8706f4de37ce952f15d2adde0776c73c159ba645
Dario Izzo <dario.izzo@gmail.com>
2014-06-19 19:35:29 +0200

    Module description improved

--- PyKEP/traj/__init__.py
+++ PyKEP/traj/__init__.py
@@ -1,5 +1,8 @@
 """
-This module contains models of interplanetary trajectories
+This module contains interplanetary trajectories problems in the form of PyGMO.problem classes. The use
+of PyGMO.archipelago and PyGMO.island with these may result in slow execution as the parallel execution will
+be handled by python multiprocessing module, which is far slower than boost::threads (used instead when the PyGMO problems
+are implemented in c++)
 """
 from PyKEP import __extensions__
 
