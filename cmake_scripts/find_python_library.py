097868b0e901bca38926e0f3c8f4a28ba9ab0306
Dario Izzo <dario.izzo@gmail.com>
2015-03-22 19:48:10 +0100

    dynamic python libraries are now preferred

--- cmake_scripts/find_python_library.py
+++ cmake_scripts/find_python_library.py
@@ -11,4 +11,10 @@ for file in os.listdir(py_lib_path):
             if file.find(py_ver) != -1:
                 available_libs.append(file)
 
-print(py_lib_path + "/" + available_libs[0])
+dynamic_libs = [d for d in available_libs if d.find('dylib') != -1 or d.find('.so') != -1 or d.find('.dll') != -1]
+static_libs = [d for d in available_libs if d.find('a')]
+
+if dynamic_libs != []:
+    print(py_lib_path + "/" + dynamic_libs[0])
+else:
+    print(py_lib_path + "/" + static_libs[0])
