3ea67d1ecd8f69cfd139b60b013278c26c52bc8b
Dario Izzo <dario.izzo@gmail.com>
2015-04-27 09:41:40 +0200

    Fix for python 3 syntax and import dependencies check improvement

--- PyKEP/phasing/__init__.py
+++ PyKEP/phasing/__init__.py
@@ -10,8 +10,8 @@ if (__extensions__['scipy']):
 if (__extensions__['scikit-learn']):
     from ._dbscan import *
 
-if (__extensions__['pygmo']):
-    from _lambert import lambert_metric
+if (__extensions__['pygmo'] and __extensions__['mplot3d']):
+    from ._lambert import lambert_metric
 
 
 def three_impulses_approx(pl1, pl2, ep1=None, ep2=None):
