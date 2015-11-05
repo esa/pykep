1513f2c7f417923cd604f5821a2b0f759de5408a
Chris Andre <chrisandre01@gmail.com>
2015-11-05 18:44:47 -0500

    fix leg_s import in sims_flanagan module

--- PyKEP/sims_flanagan/__init__.py
+++ PyKEP/sims_flanagan/__init__.py
@@ -5,7 +5,7 @@ low-thrust tajectories using our own flavour of the Sims-Flanagan model: a traje
 transcription method that forms the basis for MALTO, the software in use in JPL
 for preliminary interplanetary trajectory design.
 """
-from PyKEP.sims_flanagan._sims_flanagan import leg, spacecraft, sc_state
+from PyKEP.sims_flanagan._sims_flanagan import leg, spacecraft, sc_state, leg_s
 
 
 def _leg_get_states(self):
