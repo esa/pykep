6d638c9c8b07c591d5168cd3ebd24fcfacfc954a
Dario Izzo <dario.izzo@gmail.com>
2016-11-22 10:58:18 +0100

    towards the wheel #2

---
+++ tools/mingw_wheel_setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup
+from setuptools.dist import Distribution
+from distutils import util
+import sys
+
+NAME = 'pykep'
+VERSION = '@pykep_VERSION@'
+DESCRIPTION = 'Basic space flight mechanics computations mostly based on perturbed Keplerian dynamics'
+LONG_DESCRIPTION = 'PyKEP is a scientific library providing basic tools for space-flight-mechanics research. Algoritmic efficiency is a main focus of the library, which uses a modern algorithm backend developed by the Advanced Concepts Team at ESA. At the library core is the implementation of an efficient solver for the multiple revolutions Lambert’s problem, objects representing the Sims-Flanagan low-thrust model, efficient keplerian propagators, Taylor-integrators, a SGP4 propagator, TLE and SATCAT support and more, ....'
+URL = 'https://github.com/esa/pykep'
+AUTHOR = 'Dario Izzo'
+AUTHOR_EMAIL = 'dario.izzo@gmail.com'
+LICENSE = 'GPLv3+/LGPL3+'
+CLASSIFIERS = [
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 4 - Beta',
+
+    'Operating System :: OS Independent',
+
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Astronomy',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Scientific/Engineering :: Physics',
+
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3'
+]
+KEYWORDS = 'space keplerian math physics interplanetary'
+INSTALL_REQUIRES = ['numpy','scipy']
+PLATFORMS = ['Unix','Windows','OSX']
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(foo):
+        return True
+
+# Setup the list of external dlls.
+import os.path
+mingw_wheel_libs = 'mingw_wheel_libs_python{}.txt'.format(sys.version_info[0])
+l = open(mingw_wheel_libs,'r').readlines()
+DLL_LIST = [os.path.basename(_[:-1]) for _ in l]
+
+setup(name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url=URL,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
+    platforms=PLATFORMS,
+    install_requires=INSTALL_REQUIRES,
+    packages=['pykep'],
+    package_dir = {
+            'pykep': 'pykep',
+            },
+    # Include pre-compiled extension
+    package_data={'pykep': ['_core.pyd'] + DLL_LIST},
+    distclass=BinaryDistribution)
