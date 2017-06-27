a5198552bdc4d839e7dc6a680a2fe2bb952a452a
darioizzo <dario.izzo@gmail.com>
2017-06-27 12:09:26 +0200

    move to std::array and relaxation of the boost requirment to 1.56

--- src/astro_constants.h
+++ src/astro_constants.h
@@ -26,18 +26,18 @@
 #ifndef KEP_TOOLBOX_ASTRO_CONSTANTS_H
 #define KEP_TOOLBOX_ASTRO_CONSTANTS_H
 
-#include <vector>
-#include <string>
-#include <boost/math/constants/constants.hpp>
 #include <boost/array.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/math/constants/constants.hpp>
+#include <string>
+#include <vector>
 
 // Redefining M_PI end M_E to avoid problems in windows
 #ifndef M_PI
 #define M_PI boost::math::constants::pi<double>()
 #endif
 #ifndef M_PI_4
-#define M_PI_4 boost::math::constants::pi<double>()/4
+#define M_PI_4 boost::math::constants::pi<double>() / 4
 #endif
 #ifndef M_E
 #define M_E boost::math::constants::e<double>()
@@ -50,61 +50,59 @@
 #define ASTRO_EARTH_VELOCITY 29784.6905
 #define ASTRO_EARTH_J2 1.08262668E-03
 #define ASTRO_EARTH_RADIUS 6378137
-#define ASTRO_DEG2RAD (M_PI/180.0)
-#define ASTRO_RAD2DEG (180.0/M_PI)
-#define ASTRO_DAY2SEC 86400.0 //needs to be a double
+#define ASTRO_DEG2RAD (M_PI / 180.0)
+#define ASTRO_RAD2DEG (180.0 / M_PI)
+#define ASTRO_DAY2SEC 86400.0 // needs to be a double
 #define ASTRO_SEC2DAY (1. / 86400.0)
 #define ASTRO_DAY2YEAR (1. / 365.25)
 #define ASTRO_G0 9.80665
 #define ASTRO_CAVENDISH 73.6687e-11
 
-//This is used as a numerical proceure (e.g. newton-raphson or runge-kutta or regula-falsi) stopping criteria
+// This is used as a numerical proceure (e.g. newton-raphson or runge-kutta or
+// regula-falsi) stopping criteria
 #define ASTRO_TOLERANCE 1e-16
 
-//This is used in the Lambert Problem and in the solution of kepler equation
+// This is used in the Lambert Problem and in the solution of kepler equation
 #define ASTRO_MAX_ITER 50
 
-//This needs to be set to the precision of the boost date library (microseconds is default,
-//nanoseconds can be set when compiling boosts. Note that the code has not been checked in that case)
+// This needs to be set to the precision of the boost date library (microseconds
+// is default,
+// nanoseconds can be set when compiling boosts. Note that the code has not been
+// checked in that case)
 #define BOOST_DATE_PRECISION 1e-6
 
-namespace kep_toolbox
-{
-//Typedef for fixed size vectors
-typedef boost::array<double,3> array3D;
-typedef boost::array<double,6> array6D;
-typedef boost::array<double,7> array7D;
+namespace kep_toolbox {
+// Typedef for fixed size vectors
+typedef std::array<double, 3> array3D;
+typedef std::array<double, 6> array6D;
+typedef std::array<double, 7> array7D;
 }
 
-namespace std
-{
-	/// Overload stream insertion operator for arrayND
-	template <size_t N>
-	inline ostream &operator<<(ostream &os, const boost::array<double,N> &v)
-	{
-		os << '[';
-		for (typename boost::array<double,N>::size_type i = 0; i < v.size(); ++i) {
-			os << boost::lexical_cast<std::string>(v[i]);
-			if (i != v.size() - 1) {
-				os << ", ";
-			}
-		}
-		os << ']';
-		return os;
-	}
+namespace std {
+/// Overload stream insertion operator for arrayND
+template <size_t N>
+inline ostream &operator<<(ostream &os, const std::array<double, N> &v) {
+  os << '[';
+  for (typename std::array<double, N>::size_type i = 0; i < v.size(); ++i) {
+    os << boost::lexical_cast<std::string>(v[i]);
+    if (i != v.size() - 1) {
+      os << ", ";
+    }
+  }
+  os << ']';
+  return os;
+}
 
-	inline ostream &operator<<(ostream &os, const std::vector<double> &v)
-	{
-		os << '[';
-		for (kep_toolbox::array3D::size_type i = 0; i < v.size(); ++i) {
-			os << boost::lexical_cast<std::string>(v[i]);
-			if (i != v.size() - 1) {
-				os << ", ";
-			}
-		}
-		os << ']';
-		return os;
-	}
-
+inline ostream &operator<<(ostream &os, const std::vector<double> &v) {
+  os << '[';
+  for (kep_toolbox::array3D::size_type i = 0; i < v.size(); ++i) {
+    os << boost::lexical_cast<std::string>(v[i]);
+    if (i != v.size() - 1) {
+      os << ", ";
+    }
+  }
+  os << ']';
+  return os;
+}
 }
 #endif // KEP_TOOLBOX_ASTRO_CONSTANTS_H
--- src/core_functions/fb_vel.h
+++ src/core_functions/fb_vel.h
@@ -31,12 +31,18 @@
 
 /// Compute fly-by constraints
 /**
- * This template function can be used to evaluate the feasibility of a fly-by described by relative planetary velocities
+ * This template function can be used to evaluate the feasibility of a fly-by
+ * described by relative planetary velocities
  * before and after.
  *
- * \param[out] dV is the delta-V thrust magnitude needed to make a fly-by possible. For a ballistic fly-by dV must be zero.
- * \param[in] v_rel_in  initial position vector. On output contains the propagated position. (r0[1],r0[2],r0[3] need to be preallocated, suggested template type is boost::array<double,3))
- * \param[in] v_rel_out initial velocity vector. On output contains the propagated velocity. (v0[1],v0[2],v0[3] need to be preallocated, suggested template type is boost::array<double,3))
+ * \param[out] dV is the delta-V thrust magnitude needed to make a fly-by
+ * possible. For a ballistic fly-by dV must be zero.
+ * \param[in] v_rel_in  initial position vector. On output contains the
+ * propagated position. (r0[1],r0[2],r0[3] need to be preallocated, suggested
+ * template type is std::array<double,3))
+ * \param[in] v_rel_out initial velocity vector. On output contains the
+ * propagated velocity. (v0[1],v0[2],v0[3] need to be preallocated, suggested
+ * template type is std::array<double,3))
  * \param[in] pl a planet object
  *
  * @author Dario Izzo (dario.izzo _AT_ googlemail.com)
@@ -45,23 +51,28 @@
 
 namespace kep_toolbox {
 
-template<class vettore3D>
-inline void fb_vel(double& dV, const vettore3D& v_rel_in, const vettore3D& v_rel_out, const planet::base &pl)
-{
-    double Vin2  = v_rel_in[0]*v_rel_in[0]+v_rel_in[1]*v_rel_in[1]+v_rel_in[2]*v_rel_in[2];
-    double Vout2 = v_rel_out[0]*v_rel_out[0]+v_rel_out[1]*v_rel_out[1]+v_rel_out[2]*v_rel_out[2];
-    //eq_V2 = Vin2 - Vout2;
+template <class vettore3D>
+inline void fb_vel(double &dV, const vettore3D &v_rel_in,
+                   const vettore3D &v_rel_out, const planet::base &pl) {
+  double Vin2 = v_rel_in[0] * v_rel_in[0] + v_rel_in[1] * v_rel_in[1] +
+                v_rel_in[2] * v_rel_in[2];
+  double Vout2 = v_rel_out[0] * v_rel_out[0] + v_rel_out[1] * v_rel_out[1] +
+                 v_rel_out[2] * v_rel_out[2];
+  // eq_V2 = Vin2 - Vout2;
 
-    double e_min = 1 + pl.get_safe_radius() / pl.get_mu_self() * Vin2;
-    double alpha = acos( (v_rel_in[0]*v_rel_out[0] + v_rel_in[1]*v_rel_out[1] + v_rel_in[2]*v_rel_out[2]) / sqrt(Vin2 * Vout2) );
-    double ineq_delta = alpha - 2 * asin(1/e_min);
-    
-    if(ineq_delta > 0.0) dV = sqrt( Vout2 + Vin2 - 2.0*sqrt(Vout2*Vin2)*cos(ineq_delta) );
-    else dV = fabs(sqrt(Vout2) - sqrt(Vin2));
-    
-    return;
+  double e_min = 1 + pl.get_safe_radius() / pl.get_mu_self() * Vin2;
+  double alpha = acos((v_rel_in[0] * v_rel_out[0] + v_rel_in[1] * v_rel_out[1] +
+                       v_rel_in[2] * v_rel_out[2]) /
+                      sqrt(Vin2 * Vout2));
+  double ineq_delta = alpha - 2 * asin(1 / e_min);
+
+  if (ineq_delta > 0.0)
+    dV = sqrt(Vout2 + Vin2 - 2.0 * sqrt(Vout2 * Vin2) * cos(ineq_delta));
+  else
+    dV = fabs(sqrt(Vout2) - sqrt(Vin2));
+
+  return;
 }
 } // namespace end
 
 #endif // KEP_TOOLBOX_FB_VEL_H
-
