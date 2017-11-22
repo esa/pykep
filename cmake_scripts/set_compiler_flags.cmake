2d1ce143f4684e770df78b8df8fe32680b9741bd
darioizzo <dario.izzo@gmail.com>
2017-11-22 15:55:35 +0100

    trying get_clock time fix for windows N.11

--- cmake_scripts/set_compiler_flags.cmake
+++ cmake_scripts/set_compiler_flags.cmake
@@ -54,9 +54,7 @@ ELSEIF ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
 			SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mieee-fp")
 	ENDIF(INTEL_IEEE_COMPLIANT)
 ELSEIF ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-	# Disable the idiotic minmax macros on MSVC, some annoying warnings,
-	# and enable the bigobj option.
-	list(APPEND CMAKE_CXX_FLAGS "-DBOOST_ALL_DYN_LINK" "-DNOMINMAX" "/wd4459" "/wd4127" "/wd4702" "/bigobj")
+	#??
 ENDIF()
 
 # The following flags will be tried on all compilers
