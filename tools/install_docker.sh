5066356864dbdd7af3d72607375b94bb489b5efe
Dario Izzo <dario.izzo@gmail.com>
2019-05-08 08:26:56 +0200

    try with new docker image

--- tools/install_docker.sh
+++ tools/install_docker.sh
@@ -6,69 +6,35 @@ set -x
 # Exit on error.
 set -e
 
-CMAKE_VERSION="3.11.1"
-BOOST_VERSION="1.67.0"
-
-if [[ ${PYKEP_BUILD} == *37 ]]; then
+if [[ ${DCGP_BUILD} == *37 ]]; then
 	PYTHON_DIR="cp37-cp37m"
-elif [[ ${PYKEP_BUILD} == *36 ]]; then
+	BOOST_PYTHON_LIBRARY_NAME="libboost_python37.so"
+	PYTHON_VERSION="37"
+elif [[ ${DCGP_BUILD} == *36 ]]; then
 	PYTHON_DIR="cp36-cp36m"
-elif [[ ${PYKEP_BUILD} == *35 ]]; then
-	PYTHON_DIR="cp35-cp35m"
-elif [[ ${PYKEP_BUILD} == *34 ]]; then
-	PYTHON_DIR="cp34-cp34m"
-elif [[ ${PYKEP_BUILD} == *27 ]]; then
+	BOOST_PYTHON_LIBRARY_NAME="libboost_python36.so"
+	PYTHON_VERSION="36"
+elif [[ ${DCGP_BUILD} == *27mu ]]; then
 	PYTHON_DIR="cp27-cp27mu"
+	BOOST_PYTHON_LIBRARY_NAME="libboost_python27mu.so"
+	PYTHON_VERSION="27"
+elif [[ ${DCGP_BUILD} == *27 ]]; then
+	PYTHON_DIR="cp27-cp27m"
+	BOOST_PYTHON_LIBRARY_NAME="libboost_python27.so"
+	PYTHON_VERSION="27"
 else
-	echo "Invalid build type: ${PYKEP_BUILD}"
+	echo "Invalid build type: ${DCGP_BUILD}"
 	exit 1
 fi
 
-# HACK: for python 3.x, the include directory
-# is called 'python3.xm' rather than just 'python3.x'.
-# This confuses the build system of Boost.Python, thus
-# we create a symlink to 'python3.x'.
-cd /opt/python/${PYTHON_DIR}/include
-PY_INCLUDE_DIR_NAME=`ls`
-# If the include dir ends with 'm', create a symlink
-# without the 'm'.
-if [[ $PY_INCLUDE_DIR_NAME == *m ]]; then
-	ln -s $PY_INCLUDE_DIR_NAME `echo $PY_INCLUDE_DIR_NAME|sed 's/.$//'`
-fi
-
 cd
-mkdir install
 cd install
-
-# Install Boost
-curl -L http://dl.bintray.com/boostorg/release/${BOOST_VERSION}/source/boost_`echo ${BOOST_VERSION}|tr "." "_"`.tar.bz2 > boost_`echo ${BOOST_VERSION}|tr "." "_"`.tar.bz2
-tar xjf boost_`echo ${BOOST_VERSION}|tr "." "_"`.tar.bz2
-cd boost_`echo ${BOOST_VERSION}|tr "." "_"`
-sh bootstrap.sh --with-python=/opt/python/${PYTHON_DIR}/bin/python > /dev/null
-./bjam --toolset=gcc link=shared threading=multi cxxflags="-std=c++11" variant=release --with-python --with-serialization --with-date_time --with-test --with-system -j2 install > /dev/null
-cd ..
-
-# Install CMake
-curl -L https://github.com/Kitware/CMake/archive/v${CMAKE_VERSION}.tar.gz > v${CMAKE_VERSION}
-tar xzf v${CMAKE_VERSION} > /dev/null 2>&1
-cd CMake-${CMAKE_VERSION}/
-./configure > /dev/null
-gmake -j2 > /dev/null
-gmake install > /dev/null
-cd ..
 
 # Install and compile pykep
 cd /pykep
 cd build
-cmake -DBUILD_MAIN=no -DBUILD_PYKEP=yes -DBUILD_SPICE=yes -DBUILD_TESTS=no -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/opt/python/${PYTHON_DIR}/bin/python ../;
+cmake -DBUILD_MAIN=no -DBUILD_PYKEP=yes -DBUILD_SPICE=yes -DBUILD_TESTS=no -DCMAKE_BUILD_TYPE=Release -DBoost_PYTHON${PYTHON_VERSION}_LIBRARY_RELEASE=/usr/local/lib/${BOOST_PYTHON_LIBRARY_NAME} -DPYTHON_EXECUTABLE=/opt/python/${PYTHON_DIR}/bin/python ../;
 make -j2 install
-
-# Compile wheels
-#cd /pykep/build/wheel
-#mv ${PATH_TO_PYTHON}/lib/python${PYTHON_VERSION}/site-packages/pykep ./
-# we create a copy of the libkeplerian_toolbox.so in /usr/lib so that the system linker can find it
-# cp ${PATH_TO_PYTHON}/lib/libkeplerian_toolbox.so /usr/local/lib
-
 
 # Compile wheels
 cd wheel
