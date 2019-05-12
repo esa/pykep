7c13380960bdbdf68839a956b1aca3c067cc284b
Dario Izzo <dario.izzo@gmail.com>
2019-05-12 22:17:44 +0200

    try to not point to bost directly

--- tools/install_appveyor.py
+++ tools/install_appveyor.py
@@ -111,8 +111,8 @@ if is_python_build:
         r'-DBUILD_MAIN=no ' + \
         r'-DBUILD_TESTS=no ' + \
         r'-DBoost_INCLUDE_DIR=c:\\local\\include ' + \
-        r'-DBoost_SERIALIZATION_LIBRARY_RELEASE=c:\\local\\lib\\libboost_serialization-mgw81-mt-x64-1_70.dll ' + \
-        r'-DBoost_DATE_TIME_LIBRARY_RELEASE=c:\\local\\lib\\libboost_date_time-mgw81-mt-x64-1_70.dll ' + \
+        #r'-DBoost_SERIALIZATION_LIBRARY_RELEASE=c:\\local\\lib\\libboost_serialization-mgw81-mt-x64-1_70.dll ' + \
+        #r'-DBoost_DATE_TIME_LIBRARY_RELEASE=c:\\local\\lib\\libboost_date_time-mgw81-mt-x64-1_70.dll ' + \
         r'-DBoost_PYTHON' + python_version + r'_LIBRARY_RELEASE=c:\\local\\lib\\libboost_python' + python_version + r'-mgw81-mt-x64-1_70.dll ' + \
         r'-DPYTHON_INCLUDE_DIR=C:\\' + python_folder + r'\\include ' + \
         r'-DPYTHON_EXECUTABLE=C:\\' + python_folder + r'\\python.exe ' + \
