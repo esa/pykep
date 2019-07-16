e12588288810bf7c6de61da2cece25acdbfc687a
Dario Izzo <dario.izzo@gmail.com>
2019-07-16 10:23:04 +0200

    mga1dsm works, gym update

--- pykep/test.py
+++ pykep/test.py
@@ -32,6 +32,7 @@ from __future__ import absolute_import as _ai
 
 import unittest as _ut
 
+
 class core_functions_test_case(_ut.TestCase):
     """Test case for the core functions
 
@@ -42,7 +43,8 @@ class core_functions_test_case(_ut.TestCase):
 
     def run_epoch_test(self):
         from .core import epoch
-        ep = epoch(julian_date=0, julian_date_type='mjd2000')
+        epoch(julian_date=0, julian_date_type='mjd2000')
+
 
 class lambert_test_case(_ut.TestCase):
     """Test case for the lambert problem class
@@ -51,7 +53,79 @@ class lambert_test_case(_ut.TestCase):
 
     def runTest(self):
         from .core import lambert_problem
-        lp = lambert_problem(r1 = [1,1,0], r2 = [0,1,0], mu = 1., cw = False, max_revs = 0, tof = 0.3)
+        lambert_problem(r1=[1, 1, 0], r2=[0, 1, 0],
+                             mu=1., cw=False, max_revs=0, tof=0.3)
+
+
+class mga_1dsm_test_case(_ut.TestCase):
+    """Test case for the mga1_dsm class
+
+    """
+
+    def runTest(self):
+        self.run_construction_test()
+        self.run_decode_times_and_vinf_test()
+
+    def run_construction_test(self):
+        from .trajopt import mga_1dsm
+        # Correct use (nothrow)
+        udp = mga_1dsm()
+        self.assertEqual(udp.get_nobj(), 1)
+        udp = mga_1dsm(tof_encoding='direct', tof=[[20, 400], [20, 400]])
+        self.assertEqual(udp.get_nobj(), 1)
+        udp = mga_1dsm(tof_encoding='eta', tof=500)
+        self.assertEqual(udp.get_nobj(), 1)
+        udp = mga_1dsm(tof_encoding='alpha', tof=[20, 500])
+        self.assertEqual(udp.get_nobj(), 1)
+        udp = mga_1dsm(tof_encoding='direct', tof=[
+                       [20, 400], [20, 400]], multi_objective=True)
+        self.assertEqual(udp.get_nobj(), 2)
+
+        # Incorrect use (raise)
+        with self.assertRaises(TypeError):
+            mga_1dsm(tof_encoding='direct', tof=34)
+        with self.assertRaises(TypeError):
+            mga_1dsm(tof_encoding='direct', tof=[[400], [20, 400]])
+        with self.assertRaises(TypeError):
+            mga_1dsm(tof_encoding='direct', tof=[20, 400])
+        with self.assertRaises(TypeError):
+            mga_1dsm(tof_encoding='eta', tof=[20, 400])
+        with self.assertRaises(TypeError):
+            mga_1dsm(tof_encoding='eta', tof=[[20, 400], [20, 400]])
+        with self.assertRaises(TypeError):
+            mga_1dsm(tof_encoding='alpha', tof=4)
+        with self.assertRaises(TypeError):
+            mga_1dsm(tof_encoding='alpha', tof=[[20, 400], [20, 400]])
+
+    def run_decode_times_and_vinf_test(self):
+        from .trajopt import mga_1dsm
+        # Alpha
+        udp = mga_1dsm(tof_encoding='alpha', tof=[20, 500])
+        x = [10] + [0., 0., 1., 1., 0.5] + [0.4, 1.3, 0.5, 0.5] + [50]
+        retval = udp._decode_times_and_vinf(x)
+        self.assertAlmostEqual(retval[0][0], 25.)
+        self.assertAlmostEqual(retval[0][1], 25.)
+        self.assertAlmostEqual(retval[1], 0.)
+        self.assertAlmostEqual(retval[2], 0.)
+        self.assertAlmostEqual(retval[3], 1.)
+        # Eta
+        udp = mga_1dsm(tof_encoding='eta', tof=50)
+        x = [10] + [0., 0., 1., 1., 0.5] + [0.4, 1.3, 0.5, 0.5]
+        retval = udp._decode_times_and_vinf(x)
+        self.assertAlmostEqual(retval[0][0], 25.)
+        self.assertAlmostEqual(retval[0][1], 12.5)
+        self.assertAlmostEqual(retval[1], 0.)
+        self.assertAlmostEqual(retval[2], 0.)
+        self.assertAlmostEqual(retval[3], 1.)
+        # Direct
+        udp = mga_1dsm(tof_encoding='direct', tof=[[10, 400], [10, 400]])
+        x = [10] + [0., 0., 1., 1., 123] + [0.4, 1.3, 0.5, 321]
+        retval = udp._decode_times_and_vinf(x)
+        self.assertAlmostEqual(retval[0][0], 123)
+        self.assertAlmostEqual(retval[0][1], 321)
+        self.assertAlmostEqual(retval[1], 0.)
+        self.assertAlmostEqual(retval[2], 0.)
+        self.assertAlmostEqual(retval[3], 1.)
 
 
 def run_test_suite(level=0):
@@ -63,14 +137,14 @@ def run_test_suite(level=0):
         level(``int``): the test level (higher values run longer tests)
 
     """
-    
+
     retval = 0
     suite = _ut.TestLoader().loadTestsFromTestCase(core_functions_test_case)
     suite.addTest(lambert_test_case())
-    
+    suite.addTest(mga_1dsm_test_case())
+
     test_result = _ut.TextTestRunner(verbosity=2).run(suite)
 
-    
     if len(test_result.failures) > 0 or len(test_result.errors) > 0:
         retval = 1
     if retval != 0:
--- pykep/trajopt/_gym.py
+++ pykep/trajopt/_gym.py
@@ -1,7 +1,7 @@
 from pykep.trajopt import mga, pl2pl_N_impulses
 from pykep import planet
 
-# Some hidden variables
+# Some "private" variables, the actual gym is below.
 _seq_cassini = [planet.jpl_lp('earth'), planet.jpl_lp('venus'), planet.jpl_lp(
 'venus'), planet.jpl_lp('earth'), planet.jpl_lp('jupiter'), planet.jpl_lp('saturn')]
 class _cassini1_udp(mga):
@@ -85,18 +85,42 @@ class _emNimp_udp(pl2pl_N_impulses):
     def __repr__(self):
         return self.get_name()
 
+class _eve_mga1dsm_udp(mga_1dsm):
+    def __init__(self, tof_encoding = 'direct', t0 = [pk.epoch(0), pk.epoch(3000)], tof = [[10,500],[10,500]]):
+        super(_eve_mga1dsm_udp, self).__init__(
+            seq=[jpl_lp('earth'), jpl_lp('venus'), jpl_lp('earth')],
+            t0=t0,
+            tof=tof,
+            vinf=[0.5, 2.5],
+            add_vinf_dep=False,
+            add_vinf_arr=True,
+            tof_encoding=tof_encoding,
+            multi_objective=False
+        )
+
+# ------------------------------------------------THE GYM ----------------------------------------------#
+# ------------------------------------------------------------------------------------------------------#
+
 class gym:
     def __init__(self):
         pass
     # Problem P1: Cassini MGA, single objective, direct encoding
     cassini1 = _cassini1_udp()
     # Problem P2: Cassini MGA, single objective, alpha encoding
-    cassini1a =_cassini1a_udp()
+    cassini1_a =_cassini1a_udp()
     # Problem P3: Cassini MGA, single objective, eta encoding
-    cassini1n =_cassini1n_udp()
+    cassini1_n =_cassini1n_udp()
+
     # Problem P4: Earth-Mars, 3 impulses, single objective, alpha encoding
     em3imp = _emNimp_udp(N = 3)
     # Problem P5: Earth-Mars, 5 impulses, single objective, alpha encoding
     em5imp = _emNimp_udp(N = 5)
     # Problem P6: Earth-Mars, 7 impulses, single objective, alpha encoding
     em7imp = _emNimp_udp(N = 7)
+    
+    # Problem P7: Earth-Venus-Earth MGA1DSM, single objective , direct encoding
+    eve_mga1dsm = _eve_mga1dsm_udp(tof_encoding = 'direct', t0 = [pk.epoch(0), pk.epoch(3000)], tof = [[10,500],[10,500]])
+    # Problem P8: Earth-Venus-Earth MGA1DSM, single objective , alpha encoding
+    eve_mga1dsm_a = _eve_mga1dsm_udp(tof_encoding = 'alpha', t0 = [pk.epoch(0), pk.epoch(3000)], tof = [300, 700])
+    # Problem P9: Earth-Venus-Earth MGA1DSM, single objective , eta encoding
+    eve_mga1dsm_n = _eve_mga1dsm_udp(tof_encoding = 'eta', t0 = [pk.epoch(0), pk.epoch(3000)], tof = 700)
