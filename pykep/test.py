919554009267b2d7d478e7957f22dd0e3af19074
Dario Izzo <dario.izzo@gmail.com>
2019-07-21 12:34:51 +0200

    docs

--- pykep/test.py
+++ pykep/test.py
@@ -160,7 +160,7 @@ class gym_test_case(_ut.TestCase):
         2.55718252e-01,  5.44489098e+01, -1.54332794e+00,  1.27160729e+00,
         9.00000000e-01,  5.88481599e+02,  4.76774269e+00,  7.00000000e+01,
         1.00000000e-02,  2.20000000e+03]
-        self.assertAlmostEqual(udp.fitness(x)[0], 8511.423044703053)
+        self.assertAlmostEqual(udp.fitness(x)[0], 1511.731793212)
 
     def run_tandem_test(self):
         from .trajopt import gym
@@ -174,7 +174,7 @@ class gym_test_case(_ut.TestCase):
 
     def run_juice_test(self):
         from .trajopt import gym
-        udp = gym.tandem(prob_id = 6, constrained=False)
+        udp = gym.juice
         x = [ 8.16283083e+03,  6.41922787e-01,  6.51202691e-01,  2.51009414e+03,
         2.97841478e-01,  3.81541370e+02,  9.58572190e-01,  1.53007674e+00,
         3.06125365e-01,  1.49264351e+02,  4.10103546e+00,  2.39297670e+00,
@@ -194,7 +194,7 @@ class gym_test_case(_ut.TestCase):
         5.88391838e-01,  2.62423586e+02, -2.40594385e-02,  2.45470457e+00,
         7.25370468e-01,  3.58067954e+02,  1.47192632e+00,  1.05000000e+00,
         9.02984391e-01,  5.38436770e+02]
-        self.assertAlmostEqual(udp.fitness(x)[0], 5855.814336322983)
+        self.assertAlmostEqual(udp.fitness(x)[0], 5855.8143406005165)
 
 
 def run_test_suite(level=0):
