ef378b330f9348e206b70a85de252edcb2af6c0e
Dario Izzo <dario.izzo@gmail.com>
2019-11-22 12:46:59 +0100

    reactivate CI

--- tools/install_deps.sh
+++ tools/install_deps.sh
@@ -6,7 +6,7 @@ set -x
 # Exit on error.
 set -e
 
-if [[ "${DCGP_BUILD}" != manylinux* ]]; then
+if [[ "${PYKEP_BUILD}" != manylinux* ]]; then
     if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh;
     else
@@ -18,11 +18,11 @@ if [[ "${DCGP_BUILD}" != manylinux* ]]; then
     conda config --add channels conda-forge --force
 
     # obake-devel is needed as far as the conda package audi does not list it as a dependency
-    conda_pkgs="cmake eigen boost boost-cpp tbb tbb-devel pagmo audi symengine obake-devel"
+    conda_pkgs="cmake boost boost-cpp"
 
-    if [[ "${DCGP_BUILD}" == "Python37" || "${DCGP_BUILD}" == "OSXPython37" ]]; then
-        conda_pkgs="$conda_pkgs python=3.7 pyaudi pygmo"
-    elif [[ "${DCGP_BUILD}" == "Python27" || "${DCGP_BUILD}" == "OSXPython27" ]]; then
+    if [[ "${PYKEP_BUILD}" == "Python37" || "${PYKEP_BUILD}" == "OSXPython37" ]]; then
+        conda_pkgs="$conda_pkgs python=3.7 numpy matplotlib sklearn pygmo scipy numba"
+    elif [[ "${PYKEP_BUILD}" == "Python27" || "${PYKEP_BUILD}" == "OSXPython27" ]]; then
         conda_pkgs="$conda_pkgs python=2.7 pyaudi pygmo"
     fi
 
@@ -31,9 +31,6 @@ if [[ "${DCGP_BUILD}" != manylinux* ]]; then
     source activate $deps_dir
     conda install $conda_pkgs -y
 
-    #if [[ "${DCGP_BUILD}" == Python* ]]; then
-    #    conda install doxygen graphviz -y
-    #fi
 fi
 
 set +e
--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -12,30 +12,30 @@ if [[ "${PYKEP_BUILD}" != manylinux* ]]; then
     export PATH="$deps_dir/bin:$PATH"
 fi
 
-if [[ "${DCGP_BUILD}" == "ReleaseGCC" ]]; then
-    cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Release -DDCGP_BUILD_DCGP=yes -DDCGP_BUILD_TESTS=yes -DDCGP_BUILD_EXAMPLES=no -DCMAKE_CXX_FLAGS="-fuse-ld=gold" ../;
+if [[ "${PYKEP_BUILD}" == "ReleaseGCC" ]]; then
+    cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Release -DPYKEP_BUILD_KEP_TOOLBOX=yes -DPYKEP_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-fuse-ld=gold" ../;
     make -j2 VERBOSE=1;
     ctest -VV;
-elif [[ "${DCGP_BUILD}" == "DebugGCC" ]]; then
-    cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Debug -DDCGP_BUILD_DCGP=yes -DDCGP_BUILD_TESTS=yes -DDCGP_BUILD_EXAMPLES=no -DCMAKE_CXX_FLAGS="-fsanitize=address -fuse-ld=gold" ../;
+elif [[ "${PYKEP_BUILD}" == "DebugGCC" ]]; then
+    cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Debug -DPYKEP_BUILD_KEP_TOOLBOX=yes -DPYKEP_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-fsanitize=address -fuse-ld=gold" ../;
     make -j2 VERBOSE=1;
-    LSAN_OPTIONS=suppressions=/home/travis/build/darioizzo/dcgp/tools/lsan.supp ctest -VV;
-elif [[ "${DCGP_BUILD}" == "CoverageGCC" ]]; then
-    cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Debug -DDCGP_BUILD_DCGP=yes -DDCGP_BUILD_TESTS=yes -DDCGP_BUILD_EXAMPLES=no -DCMAKE_CXX_FLAGS="--coverage -fuse-ld=gold" ../;
+    LSAN_OPTIONS=suppressions=/home/travis/build/darioizzo/keplerian_toolbox/tools/lsan.supp ctest -VV;
+elif [[ "${PYKEP_BUILD}" == "CoverageGCC" ]]; then
+    cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Debug -DPYKEP_BUILD_KEP_TOOLBOX=yes -DPYKEP_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="--coverage -fuse-ld=gold" ../;
     make -j2 VERBOSE=1;
     ctest -VV;
     bash <(curl -s https://codecov.io/bash) -x gcov-5;
-elif [[ "${DCGP_BUILD}" == Python* ]]; then
+elif [[ "${PYKEP_BUILD}" == Python* ]]; then
     # Install dcgp
-    cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Release -DDCGP_BUILD_DCGP=yes -DDCGP_BUILD_TESTS=no ../; 
+    cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Release -DPYKEP_BUILD_KEP_TOOLBOX=yes -DPYKEP_BUILD_TESTS=no ../; 
     make install VERBOSE=1;
     # Install dcgpy.
-    cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Release -DDCGP_BUILD_DCGP=no -DDCGP_BUILD_DCGPY=yes ../; 
+    cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Release -DPYKEP_BUILD_KEP_TOOLBOX=no -DPYKEP_BUILD_PYKEP=yes ../; 
     make install VERBOSE=1;
     # Move out of the build dir.
     cd ../tools
     # Run the test suite
-    python -c "from dcgpy import test; test.run_test_suite()";
+    python -c "from pykep import test; test.run_test_suite()";
 elif [[ "${PYKEP_BUILD}" == manylinux* ]]; then
     cd ..;
     docker pull ${DOCKER_IMAGE};
