8b1225dd2bf261d09972922c89bdc412f1ef9a34
Dario Izzo <dario.izzo@gmail.com>
2019-11-26 08:28:25 +0100

    tabs 2 spaces

--- tools/install_docker.sh
+++ tools/install_docker.sh
@@ -7,20 +7,20 @@ set -x
 set -e
 
 if [[ ${PYKEP_BUILD} == *38 ]]; then
-	PYTHON_DIR="cp38-cp38"
-	BOOST_PYTHON_LIBRARY_NAME="libboost_python38.so"
-	PYTHON_VERSION="38"
+    PYTHON_DIR="cp38-cp38"
+    BOOST_PYTHON_LIBRARY_NAME="libboost_python38.so"
+    PYTHON_VERSION="38"
 elif [[ ${PYKEP_BUILD} == *37 ]]; then
-	PYTHON_DIR="cp37-cp37m"
-	BOOST_PYTHON_LIBRARY_NAME="libboost_python37.so"
-	PYTHON_VERSION="37"
+    PYTHON_DIR="cp37-cp37m"
+    BOOST_PYTHON_LIBRARY_NAME="libboost_python37.so"
+    PYTHON_VERSION="37"
 elif [[ ${PYKEP_BUILD} == *36 ]]; then
-	PYTHON_DIR="cp36-cp36m"
-	BOOST_PYTHON_LIBRARY_NAME="libboost_python36.so"
-	PYTHON_VERSION="36"
+    PYTHON_DIR="cp36-cp36m"
+    BOOST_PYTHON_LIBRARY_NAME="libboost_python36.so"
+    PYTHON_VERSION="36"
 else
-	echo "Invalid build type: ${PYKEP_BUILD}"
-	exit 1
+    echo "Invalid build type: ${PYKEP_BUILD}"
+    exit 1
 fi
 
 cd
@@ -30,23 +30,23 @@ cd install
 cd /pykep
 cd build_kep_toolbox
 cmake -DBoost_NO_BOOST_CMAKE=ON \
-	  -DPYKEP_BUILD_KEP_TOOLBOX=yes \
-	  -DPYKEP_BUILD_PYKEP=no \
-	  -DPYKEP_BUILD_SPICE=yes \
-	  -DPYKEP_BUILD_TESTS=no \
-	  -DCMAKE_BUILD_TYPE=Release \
+      -DPYKEP_BUILD_KEP_TOOLBOX=yes \
+      -DPYKEP_BUILD_PYKEP=no \
+      -DPYKEP_BUILD_SPICE=yes \
+      -DPYKEP_BUILD_TESTS=no \
+      -DCMAKE_BUILD_TYPE=Release \
 make -j2 install
 
 # Install and compile pykep
 cd /pykep
 cd build_pykep
 cmake -DBoost_NO_BOOST_CMAKE=ON \
-	  -DPYKEP_BUILD_KEP_TOOLBOX=no \
-	  -DPYKEP_BUILD_PYKEP=yes \
-	  -DPYKEP_BUILD_TESTS=no \
-	  -DCMAKE_BUILD_TYPE=Release \
-	  -DBoost_PYTHON${PYTHON_VERSION}_LIBRARY_RELEASE=/usr/local/lib/${BOOST_PYTHON_LIBRARY_NAME} \
-	  -DPYTHON_EXECUTABLE=/opt/python/${PYTHON_DIR}/bin/python ../;
+      -DPYKEP_BUILD_KEP_TOOLBOX=no \
+      -DPYKEP_BUILD_PYKEP=yes \
+      -DPYKEP_BUILD_TESTS=no \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBoost_PYTHON${PYTHON_VERSION}_LIBRARY_RELEASE=/usr/local/lib/${BOOST_PYTHON_LIBRARY_NAME} \
+      -DPYTHON_EXECUTABLE=/opt/python/${PYTHON_DIR}/bin/python ../;
 make -j2 install
 
 # Compile wheels
@@ -67,9 +67,9 @@ cd /
 # This variable will contain something if this is a tagged build (vx.y.z), otherwise it will be empty.
 export PYKEP_RELEASE_VERSION=`echo "${TRAVIS_TAG}"|grep -E 'v[0-9]+\.[0-9]+.*'|cut -c 2-`
 if [[ "${PYKEP_RELEASE_VERSION}" != "" ]]; then
-	echo "Release build detected, uploading to PyPi."
-	/opt/python/${PYTHON_DIR}/bin/pip install twine
-	/opt/python/${PYTHON_DIR}/bin/twine upload -u darioizzo /pykep/build/wheel/dist2/pykep*
+    echo "Release build detected, uploading to PyPi."
+    /opt/python/${PYTHON_DIR}/bin/pip install twine
+    /opt/python/${PYTHON_DIR}/bin/twine upload -u darioizzo /pykep/build/wheel/dist2/pykep*
 fi
 
 
