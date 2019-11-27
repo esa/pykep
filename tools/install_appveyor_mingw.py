da1b7e647c6ac897cfccdee3f988391a206f68aa
Dario Izzo <dario.izzo@gmail.com>
2019-11-27 10:21:46 +0100

    Appveyor MinGW Try 14.

--- tools/install_appveyor_mingw.py
+++ tools/install_appveyor_mingw.py
@@ -105,7 +105,7 @@ common_cmake_opts = r'-DCMAKE_PREFIX_PATH=c:\\local ' + \
                     r'-DCMAKE_INSTALL_PREFIX=c:\\local ' + \
                     r'-DPYKEP_BUILD_SPICE=yes ' + \
                     r'-DBoost_SERIALIZATION_LIBRARY_RELEASE=c:\\local\\lib\\libboost_serialization-mgw81-mt-x64-1_70.dll ' + \
-                    r'-DBoost_INCLUDE_DIR=c:\\local\\include' + \
+                    r'-DBoost_INCLUDE_DIR=c:\\local\\include ' + \
                     r'-DBoost_DATE_TIME_LIBRARY_RELEASE=c:\\local\\lib\\libboost_date_time-mgw81-mt-x64-1_70.dll'
 
 # Configuration step.
