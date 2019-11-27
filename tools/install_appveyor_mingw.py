49740cdd2bf87c1691b459dbb1430fc757dbcdc0
Dario Izzo <dario.izzo@gmail.com>
2019-11-27 18:05:30 +0100

    py38 in minGW

--- tools/install_appveyor_mingw.py
+++ tools/install_appveyor_mingw.py
@@ -73,16 +73,10 @@ if is_python_build:
         python_version = '36'
         python_folder = r'Python36-x64'
         python_library = r'C:\\' + python_folder + r'\\python36.dll '
-    elif 'Python27-x64' in BUILD_TYPE:
-        python_version = r'27'
-        python_folder = r'Python27-x64'
-        python_library = r'C:\\' + python_folder + r'\\libs\\python27.dll '
-        # Fot py27 I could not get it to work with the appveyor python (I was close but got tired).
-        # Since this is anyway going to disappear (py27 really!!!), I am handling it as a one time workaround using the old py27 patched by bluescarni
-        rm_fr(r'c:\\Python27-x64')
-        wget(r'https://github.com/bluescarni/binary_deps/raw/master/python27_mingw_64.7z', 'python.7z')
-        run_command(r'7z x -aoa -oC:\\ python.7z', verbose=False)
-        run_command(r'mv C:\\Python27 C:\\Python27-x64', verbose=False)
+    elif 'Python38-x64' in BUILD_TYPE:
+        python_version = r'38'
+        python_folder = r'Python38-x64'
+        python_library = r'C:\\' + python_folder + r'\\python38.dll '
     else:
         raise RuntimeError('Unsupported Python build: ' + BUILD_TYPE)
 
