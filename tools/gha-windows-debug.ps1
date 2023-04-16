be1b56b3099cfe2477b71ea2db8ca17176886d10
Dario Izzo <dario.izzo@gmail.com>
2023-04-16 22:36:33 +0200

    add CI for win N.7

--- tools/gha-windows-debug.ps1
+++ tools/gha-windows-debug.ps1
@@ -4,12 +4,23 @@ conda config --set always_yes yes
 conda create --name pykep cmake boost boost-cpp python=3.11 scipy matplotlib
 conda activate pykep
 
+# Define environment variables for clang ...
+# ... and make them persistent 
+cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat`" && set > %temp%\vcvars.txt"
+
+Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
+  if ($_ -match "^(.*?)=(.*)$") {
+    Set-Content "env:\$($matches[1])" $matches[2]
+  }
+}
+
 mkdir build
 cd build
 
 cmake `
-    -G "Visual Studio 16 2019" `
-    -A x64 `
+    -G "Ninja" `
+    -DCMAKE_C_COMPILER=clang-cl `
+    -DCMAKE_CXX_COMPILER=clang-cl `
     -DCMAKE_PREFIX_PATH=C:\Miniconda\envs\pykep `
     -DCMAKE_INSTALL_PREFIX=C:\Miniconda\envs\pykep `
     -DBoost_NO_BOOST_CMAKE=ON `
