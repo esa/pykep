41152be33fd601e8691958e5cf5159312a43a152
Dario Izzo <dario.izzo@esa.int>
2023-04-17 13:54:48 +0200

    Win trial

--- tools/gha-windows-debug.ps1
+++ tools/gha-windows-debug.ps1
@@ -4,31 +4,34 @@ conda config --set always_yes yes
 conda create --name pykep cmake boost boost-cpp python=3.11 scipy matplotlib
 conda activate pykep
 
-# Define environment variables for clang ...
-# ... and make them persistent 
-cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat`" && set > %temp%\vcvars.txt"
-
-Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
-  if ($_ -match "^(.*?)=(.*)$") {
-    Set-Content "env:\$($matches[1])" $matches[2]
-  }
-}
-
 mkdir build
 cd build
 
 cmake `
-    -G "Ninja" `
-    -DCMAKE_C_COMPILER=clang-cl `
-    -DCMAKE_CXX_COMPILER=clang-cl `
+    -G "Visual Studio 16 2019" -A x64 `
+    -DBoost_NO_BOOST_CMAKE=ON `
     -DCMAKE_PREFIX_PATH=C:\Miniconda\envs\pykep `
     -DCMAKE_INSTALL_PREFIX=C:\Miniconda\envs\pykep `
-    -DBoost_NO_BOOST_CMAKE=ON `
-    -DPYKEP_BUILD_KEP_TOOLBOX=yes `
-    -DPYKEP_BUILD_PYKEP=no `
+    -DPYKEP_BUILD_TESTS=no `
     -DPYKEP_BUILD_SPICE=yes `
-    -DPYKEP_BUILD_TESTS=yes `
     ..
 
-cmake --build . --target install --config Release
-ctest -j4 -V -C Release
+cmake --build . --config Release
+cmake --build . --config Release --target install
+
+
+cd ..
+mkdir build_python
+cd build_python
+
+cmake `
+    -G "Visual Studio 16 2019" -A x64 `
+    -DBoost_NO_BOOST_CMAKE=ON `
+    -DCMAKE_PREFIX_PATH=C:\Miniconda\envs\pykep `
+    -DCMAKE_INSTALL_PREFIX=C:\Miniconda\envs\pykep `
+    -DPYKEP_BUILD_PYKEP=yes `
+    -DPYKEP_BUILD_KEP_TOOLBOX=no `
+    ..
+
+cmake --build . --config Release
+cmake --build . --config Release --target install
