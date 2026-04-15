b341a70a5940fb3c0d81d08277cf4176e1076d1f
Dario Izzo <dario.izzo@gmail.com>
2026-04-15 17:35:28 +0200

    reactivating runtime deps

--- tools/gha_deploydocs.sh
+++ tools/gha_deploydocs.sh
@@ -11,8 +11,8 @@ DEPS_DIR="$HOME/local"
 # Install build and docs deps into a dedicated conda environment.
 conda create -y -q -p "$DEPS_DIR" \
     c-compiler cxx-compiler cmake ninja \
-    "libboost>=1.73" "fmt>=10" "heyoka>=7" spdlog \
-    "xtensor>=0.26" xtensor-blas pagmo-devel \
+    "libboost>=1.73" "fmt>=10" "heyoka>=7" "heyoka.py>=7" spdlog \
+    "xtensor>=0.26" xtensor-blas pagmo-devel pygmo \
     pybind11 sgp4 spiceypy matplotlib scipy \
     python=3.13 \
     "sphinx<9" sphinx-book-theme sphinxcontrib-bibtex myst-nb graphviz
