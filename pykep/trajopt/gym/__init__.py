91d3262dc4584c592832d0587497067eebde9bd5
Dario Izzo <dario.izzo@gmail.com>
2026-04-15 20:39:26 +0200

    adding tops db

--- pykep/trajopt/gym/__init__.py
+++ pykep/trajopt/gym/__init__.py
@@ -5,3 +5,20 @@ from ._rosetta_mga_1dsm import rosetta
 from ._juice_mga_1dsm import juice, juice_mo
 from ._messenger import messenger
 from ._em_Nimp import em3imp, em5imp, em7imp
+
+
+# Load TOPS benchmark --------------------------------------------------------
+import json
+from importlib import resources
+def _load_tops_json(name: str):
+    with resources.as_file(resources.files(__package__) / "tops" / name) as _path:
+        with _path.open("r", encoding="utf-8") as _f:
+            return json.load(_f)
+
+
+zoh_cr3bp = _load_tops_json("_tops_cr3bp.json")
+zoh_twobody = _load_tops_json("_tops_twobody.json")
+zoh_ss = _load_tops_json("_tops_ss.json")
+zoh_mee = _load_tops_json("_tops_mee.json")
+
+del _load_tops_json, resources, json
